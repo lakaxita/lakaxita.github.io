eb97b8bcc281ad2d76e0a44af808f46be4c0169b
Unai Zalakain <unai@gisa-elkartea.org>
2014-03-20 20:58:05 +0100

    Try to build compass automatically on github

--- static/config.rb
+++ static/config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 add_import_path "components/foundation/scss"
-require "zurb-foundation"
+require ['jekyll-compass', "zurb-foundation"]
 
 # Set this to the root of your project when deployed:
 http_path = "/static"
