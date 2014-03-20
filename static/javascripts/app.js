84f6df31165f5044b84d6f618f43e1deb22097d1
Unai Zalakain <unai@gisa-elkartea.org>
2014-03-20 18:29:18 +0100

    Use anchors on nav post category links

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
-    $('nav > a.posts').click(function(event) {
-        event.preventDefault();
+function filter_posts(category) {
+    if (category.length != 0) {
         var articles = $('section#posts > article');
         articles.hide();
-        articles.filter('.'+$(this).text()).show();
+        articles.filter('.'+category).show();
+    };
+};
+
+$(document).ready(function() {
+    var category = window.location.hash.replace('#','');
+    filter_posts(category);
+    $('nav a.posts').click(function(event) {
+        var category = $(this).text();
+        filter_posts(category);
     });
 });
